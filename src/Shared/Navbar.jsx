0427f0b2d89126c983ea3c1a9d24e8ce93d38328
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-10-20 16:57:52 +0600

    link add

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
         <Link to='/'><img src={logo} alt="" className='w-32 md:w-[180px]' /></Link>
       </div>
       <div className='flex gap-5 items-center'>
-        <button className='uppercase font-semibold'>login</button>
+        <Link to='/login' className='uppercase font-semibold'>login</Link>
         <span className='text-xl font-thin mb-1.5'>|</span>
-        <button className='uppercase font-semibold'>register</button>
+        <Link to='/register' className='uppercase font-semibold'>register</Link>
       </div>
     </nav>
   )
