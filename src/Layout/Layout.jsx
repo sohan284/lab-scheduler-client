cec1a0d489eb0097fcdc43ec13d3a7f3dfe4674c
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-10-20 16:42:29 +0600

    Navbar , tabnav, nad scheduled tasj=k added

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Shared/Navbar';
+import TabNav from '../Shared/TabNav';
 
 const Layout = () => {
     return (
         <div>
+            <Navbar />
+            <TabNav />
             <Outlet />
         </div>
     );
