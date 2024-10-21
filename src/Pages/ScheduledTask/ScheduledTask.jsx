35b2c849d64ed03acec0f2a1f5f837a53a0413ec
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-10-21 09:41:01 +0600

    Schedule added

--- src/Pages/ScheduledTask/ScheduledTask.jsx
+++ src/Pages/ScheduledTask/ScheduledTask.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ScheduledTask = () => {
     return (
-        <div>
+        <div className='px-4 py-8'>
             <h1 className='md:text-xl font-bold uppercase mb-6 '>Scheduled Tasks</h1>
             <div className='flex justify-between'> 
-                <div className="flex justify-between items-center pb-2">
+                <div className="w-1/2">
                     <h1>Task Name </h1>
                 </div>
-                <div>
+                <div className='w-1/2'>
                     <p>1040</p>
                 </div>
             </div>
