a841623af4e3f151f61ca58bb58af8c8440c1411
hasib1010 <asifmahmud664@gmail.com>
2024-10-23 16:23:22 +0600

    updated

--- src/Pages/AddedTasks/AddedTasks.jsx
+++ src/Pages/AddedTasks/AddedTasks.jsx
@@ -37,60 +37,60 @@ const AddedTasks = () => {
   };
 
   return (
-  <>
-    <TabNav/>
-    <div className="px-4 py-10">
-      <h1 className="md:text-xl font-bold uppercase mb-6">Scheduled Tasks</h1>
-      {loading ? (
-        <Loader text={"Collecting Your Data"} />
-      ) : (
-        <div>
-          {tasks.length > 0 ? (
-            <div className="   ">
-              {tasks.map((task, index) => (
-                <div key={index} className="flex justify-between mb-20">
-                  <div className="w-1/2">
-                    <h1 className="py-4 px-10 font-bold">Task Name</h1>
-                    <h1 className="bg-zinc-50 py-4 px-10">Machine</h1>
-                    <h1 className="py-4 px-10">
-                      Estimated time required to finish the task
-                    </h1>
-                    <h1 className="bg-zinc-50 py-4 px-10 font-bold">
-                      Available time slot
-                    </h1>
-                    <h1 className="py-4 px-10">
-                      This machine requires faculty permission/availability.
-                    </h1>
-                    <button className="py-4 px-10 underline underline-offset-4">
-                      Tutorial
-                    </button>
-                    <button className="block py-4 px-10 underline underline-offset-4">
-                      Share
-                    </button>
+    <>
+      <TabNav />
+      <div className="px-4 py-10 text-xs">
+        <h1 className="  font-bold uppercase mb-6">Scheduled Tasks</h1>
+        {loading ? (
+          <Loader text={"Collecting Your Data"} />
+        ) : (
+          <div>
+            {tasks.length > 0 ? (
+              <div className="   ">
+                {tasks.map((task, index) => (
+                  <div key={index} className="flex justify-between mb-10 border-b-2 border-dashed border-gray-300">
+                    <div className="w-1/2">
+                      <h1 className="py-4 px-10 font-bold">Task Name</h1>
+                      <h1 className="bg-zinc-50 py-4 px-10">Machine</h1>
+                      <h1 className="py-4 px-10">
+                        Estimated time required to finish the task
+                      </h1>
+                      <h1 className="bg-zinc-50 py-4 px-10 font-bold">
+                        Available time slot
+                      </h1>
+                      <h1 className="py-4 px-10">
+                        This machine requires faculty permission/availability.
+                      </h1>
+                      <button className="py-4 px-10 underline underline-offset-4">
+                        Tutorial
+                      </button>
+                      <button className="block py-4 px-10 underline underline-offset-4">
+                        Share
+                      </button>
+                    </div>
+                    <div className="w-1/2">
+                      <p className="py-4 px-10">{task.taskName}</p>
+                      <p className="bg-zinc-50 py-4 px-10">
+                        {task.selectedMachine.join(", ")}
+                      </p>
+                      <p className="py-4 px-10">{task.estimatedTime}</p>
+                      <p className="bg-zinc-50 py-4 px-10 font-bold">
+                        {task?.startDate
+                          ? `${formatDate(task.startDate)}, ${task.selectedTimeSlots[0]
+                          }`
+                          : "N/A"}
+                      </p>
+                    </div>
                   </div>
-                  <div className="w-1/2">
-                    <p className="py-4 px-10">{task.taskName}</p>
-                    <p className="bg-zinc-50 py-4 px-10">
-                      {task.selectedMachine.join(", ")}
-                    </p>
-                    <p className="py-4 px-10">{task.estimatedTime}</p>
-                    <p className="bg-zinc-50 py-4 px-10 font-bold">
-                      {task?.startDate
-                        ? `${formatDate(task.startDate)}, ${task.selectedTimeSlots[0]
-                        }`
-                        : "N/A"}
-                    </p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <p className="text-red-600 font-bold">No Task Approved</p>
-          )}
-        </div>
-      )}
-    </div>
-  </>
+                ))}
+              </div>
+            ) : (
+              <p className="text-red-600 font-bold">No Task Approved</p>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
