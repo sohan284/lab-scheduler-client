31864ff66c48ffd7b36bb75cb74ad64cc0ecc71b
sr sohan <sr.sohan088@gmail.com>
2024-11-12 09:35:02 +0600

    add vercel

--- src/api/domain.jsx
+++ src/api/domain.jsx
@@ -2,6 +2,9 @@
 // const domain = `http://localhost:5000`;
 
 // development
-const domain = `https://lab-scheduler-server.vercel.app`;
+// const domain = `https://lab-scheduler-server.vercel.app`;
+
+// production 
+const domain = `https://lab-scheduler-server-roan.vercel.app`
 
 export default domain;
