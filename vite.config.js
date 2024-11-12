28c5d2c996d2003f3116535c0e8b90aed08c0289
sr sohan <sr.sohan088@gmail.com>
2024-11-12 09:57:50 +0600

    and vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // This is the default, but you can change it if needed
-  }
 })
